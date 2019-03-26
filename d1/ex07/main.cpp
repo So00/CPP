ccb2510079f3b2d500d51cf91a0173ee1bffb6a6
Anthony TOURNERIE <atourner@e1r6p12.42.fr>
2019-03-26 13:39:07 +0100

    ex07 clean

--- d1/ex07/main.cpp
+++ d1/ex07/main.cpp
@@ -6,19 +6,42 @@
 /*   By: atourner <atourner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/26 11:54:26 by atourner          #+#    #+#             */
-/*   Updated: 2019/03/26 13:12:40 by atourner         ###   ########.fr       */
+/*   Updated: 2019/03/26 13:38:14 by atourner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+std::string    replace(std::string string_to_search, std::string string_to_add, std::string all_file)
+{
+    int     pos;
+    int     length_replace;
+    int     length;
+
+    pos = 0;
+    length = string_to_search.length();
+    length_replace = string_to_add.length();
+    while (all_file.find(string_to_search, pos) != std::string::npos)
+    {
+        pos = all_file.find(string_to_search, pos);
+        all_file.replace(pos, length, string_to_add);
+        pos += length_replace;
+    }
+    return (all_file);
+}
+
+void        print(std::string output, std::string print)
+{
+    output += ".replace";
+    std::ofstream fileWrite(output);
+    fileWrite << print << std::endl;
+}
+
 int     main(int ac, char **av)
 {
     int     length;
     char    *buf;
-    int     pos;
-    int     lengthReplace;
 
     if (ac != 4)
         std::cout << "Usage : ./not-sed filename string_to_search string_replacement" << std::endl;
@@ -26,29 +49,17 @@ int     main(int ac, char **av)
     {
         std::ifstream   file(av[1], std::ios::binary | std::ios::ate);
         if (file.fail())
-        {
             std::cout << "Please enter a valid file name" << std::endl;
-        }
         else
         {
             length = file.tellg();
-            file.seekg(0);
             buf = new char [length];
+            file.seekg(0);
             file.read(buf, length);
-            std::cout << buf << std::endl;
             std::string     all_file = buf;
             delete [] buf;
-            pos = 0;
-            length = std::strlen(av[2]);
-            lengthReplace = std::strlen(av[3]);
-            while (all_file.find(av[2], pos) != std::string::npos)
-            {
-                all_file.replace(all_file.find(av[2], pos), 2, av[3]);
-                pos = all_file.find(av[2], pos) + lengthReplace;
-            }
-            std::cout << all_file << std::endl;
-        }
-        
+            print(av[1], replace(av[2], av[3], all_file));      
+        }  
     }
     return (0);
 }

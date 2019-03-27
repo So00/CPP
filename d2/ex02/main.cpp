c1c652e8bca88448d4d94778c8a172c705f053be
Anthony TOURNERIE <atourner@e1r7p10.42.fr>
2019-03-27 12:31:23 +0100

    d2

--- d2/ex02/main.cpp
+++ d2/ex02/main.cpp
@@ -4,6 +4,7 @@ int main(void)
 {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    Fixed c;
     std::cout << a << std::endl;
     std::cout << "> true " << (Fixed(2.25f) > Fixed(2)) << std::endl;
     std::cout << "> false " << (Fixed(0.25f) > Fixed(2)) << std::endl;
@@ -17,12 +18,16 @@ int main(void)
     std::cout << "== false " << (Fixed(2.01f) == Fixed(2)) << std::endl;
     std::cout << "!= true " << (Fixed(2.1f) != Fixed(2)) << std::endl;
     std::cout << "!= false " << (Fixed(2.0f) != Fixed(2)) << std::endl;
-    /* std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;*/
-    std::cout << b << std::endl;
-    /*
-    std::cout << Fixed::max( a, b ) << std::endl; */
-    return 0;
+    std::cout << "a " << ++a << std::endl;
+    std::cout << "a " << a << std::endl;
+    std::cout << "a " << a++ << std::endl;
+    std::cout << "a " << a << std::endl;
+    std::cout << "c " << --c << std::endl;
+    std::cout << "c " << c << std::endl;
+    std::cout << "c " << c-- << std::endl;
+    std::cout << "c " << c << std::endl;
+    std::cout << "b " << b << std::endl;
+    std::cout << "max (a, b) " << Fixed::max( a, b ) << std::endl;
+    std::cout << "min (a, b)" << Fixed::min( a, b ) << std::endl;
+    return (0);
 }
---
+++ d2/ex04/main.cpp
@@ -0,0 +1,55 @@
+#include "Fixed.class.hpp"
+
+std::string     get_brackets(std::string str)
+{
+    size_t  pos;
+    int     brackets;
+
+    pos = str.find("(") + 1;
+    for (brackets = 1; brackets && pos < str.length(); pos++)
+    {
+        if (str[pos] == ')')
+            brackets--;
+        else if (str[pos] == '(')
+            brackets++;
+    }
+    if (brackets)
+        return (str.replace(str.begin(), str.end(), ""));
+    return (str.substr(str.find("(") + 1, pos - str.find("(") - 1));
+}
+
+float              solve(std::string str)
+{
+    return (1);
+}
+
+std::string        fire_everything(std::string str)
+{
+    std::string ret;
+
+    if (str.find("(") != std::string::npos)
+    {
+        ret = get_brackets(str);
+        if (ret.length() == 0)
+            return (ret);
+        fire_everything(ret);
+    }
+    float oneFixed = solve(str);
+    return (str);
+}
+
+/*"(15 - 15) * (2 + 15)"
+    "1 + 5 - 9 / 3 * 2"
+    "1 + 5 - 3 * 2"
+    "1 + 5 - 6"
+    "6 - 6"
+    "0"*/
+
+int     main(int ac, char **av)
+{
+    if (ac != 2)
+        std::cout << "Usage : ./eval_expr Arg" << std::endl;
+    else
+        fire_everything(av[1]);
+    return (0);
+}

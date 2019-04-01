c4c1e220f9a1049c6d4b06aa34424e6006133a76
Anthony TOURNERIE <atourner@e1r8p14.42.fr>
2019-04-01 13:47:10 +0200

    d5

---
+++ d5/ex01/Bureaucrat.hpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atourner <atourner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/01 09:59:53 by atourner          #+#    #+#             */
+/*   Updated: 2019/04/01 13:41:17 by atourner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+
+    public: 
+
+    class GradeTooHighException : public std::exception
+    {
+
+        public:
+
+            GradeTooHighException(void) throw();
+            GradeTooHighException(GradeTooHighException const & src) throw(); 
+            virtual ~GradeTooHighException(void) throw();
+            virtual const char *what() const throw();
+
+            GradeTooHighException &		operator=(GradeTooHighException const & rhs);
+
+        private:
+
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+
+        public: 
+
+            GradeTooLowException(void) throw();
+            GradeTooLowException(GradeTooLowException const & src) throw(); 
+            virtual ~GradeTooLowException(void) throw();
+            virtual const char *what() const throw();
+
+            GradeTooLowException &		operator=(GradeTooLowException const & rhs);
+
+
+    private:
+
+};
+
+    Bureaucrat(std::string const name, int grade);
+    Bureaucrat(Bureaucrat const & src);
+    ~Bureaucrat(void);
+
+	Bureaucrat &		operator=(Bureaucrat const & rhs);
+
+    std::string const   toString(void) const;
+    std::string const   getName() const;
+    int                 getGrade() const;
+
+
+    void                operator++();
+    void                operator--();
+    void                check(int grade);
+
+private:
+    Bureaucrat(void);
+    std::string const    _name;
+    int                  _grade;
+
+};
+
+std::ostream &	operator<< (std::ostream & o, Bureaucrat const & rhs);
+
+#endif
---
+++ d5/ex01/main.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atourner <atourner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/01 10:00:00 by atourner          #+#    #+#             */
+/*   Updated: 2019/04/01 13:38:59 by atourner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat  max = Bureaucrat("Max", 0);
+
+        std::cout << max << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        Bureaucrat  min = Bureaucrat("Min", 150);
+
+        std::cout << min << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        Bureaucrat  hermes = Bureaucrat("Hermes", 151);
+
+        std::cout << hermes << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        Bureaucrat  hermes = Bureaucrat("Hermes", 36);
+
+        std::cout << hermes << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        Bureaucrat  hermes = Bureaucrat("Hermes", 1);
+        --hermes;
+
+        std::cout << hermes << std::endl;
+    }
+    
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        Bureaucrat  hermes = Bureaucrat("Hermes", 150);
+        ++hermes;
+
+        std::cout << hermes << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return (0);
+}
